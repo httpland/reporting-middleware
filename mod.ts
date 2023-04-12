b4ddf4c03bf867caa72a7ee94647438eb688dfbf
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-04-12 16:55:19 +0900

    feat(mod): export related types

--- mod.ts
+++ mod.ts
@@ -3,3 +3,4 @@
 
 export { reportingEndpoints, reportTo } from "./middleware.ts";
 export { type Handler, type Middleware } from "./deps.ts";
+export type { Endpoint, EndpointGroup } from "./types.ts";
