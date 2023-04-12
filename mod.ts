0b410b1ca22bbeee2fce510d09b7a2bae3487b2b
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-04-12 16:06:05 +0900

    feat(mod): export reportTo function

--- mod.ts
+++ mod.ts
@@ -1,5 +1,5 @@
 // Copyright 2023-latest the httpland authors. All rights reserved. MIT license.
 // This module is browser compatible.
 
-export { reportingEndpoints } from "./middleware.ts";
+export { reportingEndpoints, reportTo } from "./middleware.ts";
 export { type Handler, type Middleware } from "./deps.ts";
