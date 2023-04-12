ea14db122bb9c43613ab24954c0e09bd017314ec
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-04-12 16:42:23 +0900

    docs(middleware): add code block

--- middleware.ts
+++ middleware.ts
@@ -7,7 +7,27 @@ import { ReportingHeader } from "./constants.ts";
 import type { EndpointGroup } from "./types.ts";
 
 /** Create `Reporting-Endpoints` header field middleware.
- * @throws {Error}
+ *
+ * @example
+ * ```ts
+ * import {
+ *   type Handler,
+ *   reportingEndpoints,
+ * } from "https://deno.land/x/reporting_middleware@$VERSION/mod.ts";
+ * import { assert } from "https://deno.land/std/testing/asserts.ts";
+ *
+ * declare const request: Request;
+ * declare const handler: Handler;
+ *
+ * const middleware = reportingEndpoints({
+ *   default: "https://test.test/report",
+ * });
+ * const response = await middleware(request, handler);
+ *
+ * assert(response.headers.has("reporting-endpoints"));
+ * ```
+ *
+ * @throws {Error} If the endpoints include invalid member.
  */
 export function reportingEndpoints(
   endpoints: Record<string, string> | readonly [string, string][],
@@ -26,7 +46,30 @@ export function reportingEndpoints(
 }
 
 /** Create `Report-To` header field middleware.
- * @throws {Error}
+ *
+ * @example
+ * ```ts
+ * import {
+ *   type Handler,
+ *   reportTo,
+ * } from "https://deno.land/x/reporting_middleware@$VERSION/mod.ts";
+ * import { assert } from "https://deno.land/std/testing/asserts.ts";
+ *
+ * declare const request: Request;
+ * declare const handler: Handler;
+ *
+ * const middleware = reportTo([
+ *   {
+ *     endpoints: [{ url: "https://test.test/report" }],
+ *     max_age: 86400,
+ *   },
+ * ]);
+ * const response = await middleware(request, handler);
+ *
+ * assert(response.headers.has("reporting-to"));
+ * ```
+ *
+ * @throws {Error} If the {@link EndpointGroup} includes invalid field.
  */
 export function reportTo(groups: readonly EndpointGroup[]): Middleware {
   const fieldValue = stringifyGroups(groups);
